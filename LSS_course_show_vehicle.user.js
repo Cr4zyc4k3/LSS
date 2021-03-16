378252b3b876e0e2194e44f4fd03ffc469dae1d6
Cr4zyc4k3 <69073372+Cr4zyc4k3@users.noreply.github.com>
2021-03-16 17:20:56 +0100

    🔥 fixed typos

--- LSS_course_show_vehicle.user.js
+++ LSS_course_show_vehicle.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        LSS Course show vehicles
-// @version     1.0.2
+// @version     1.0.3
 // @author      Crazycake
 // @include     /^https?:\/\/(?:w{3}\.)?(?:polizei.)?(?:leitstellenspiel\.de)\/buildings\/\d+$
 // @include     /^https?:\/\/(?:w{3}\.)?(?:polizei.)?(?:leitstellenspiel\.de)\/schoolings\/\d+$
@@ -8,6 +8,45 @@
 // @UpdateURL   https://github.com/Cr4zyc4k3/LSS/raw/main/LSS_building_notes.user.js
 // ==/UserScript==
 
+if (!sessionStorage.aVehicles || JSON.parse(sessionStorage.aVehicles).lastUpdate < (new Date().getTime() - 5 * 1000 * 60) || JSON.parse(sessionStorage.aVehicles).userId != user_id)
+{
+    await $.getJSON('/api/vehicles').done(data => sessionStorage.setItem('aVehicles', JSON.stringify({ lastUpdate: new Date().getTime(), value: data, userId: user_id })));
+}
+var aVehicles = JSON.parse(sessionStorage.aVehicles).value;
+var educationTypeGlobal;
+const educationArray = [
+    [ "gw_messtechnik", [ 12 ] ],
+    [ "gw_gefahrgut", [ 27, 77 ] ],
+    [ "gw_hoehenrettung", [ 33 ] ],
+    [ "elw2", [ 34, 78 ] ],
+    [ "wechsellader", [ 46 ] ],
+    [ "dekon_p", [ 53, 54 ] ],
+    [ "fwk", [ 57 ] ],
+    [ "arff", [ 75 ] ],
+    [ "rettungstreppe", [ 76 ] ],
+    [ "werkfeuerwehr", [ 83, 85, 86 ] ],
+    [ "notarzt", [ 29, 73, 74, 97 ] ],
+    [ "lna", [ 55 ] ],
+    [ "orgl", [ 56 ] ],
+    [ "seg_elw", [ 59 ] ],
+    [ "seg_gw_san", [ 60 ] ],
+    [ "gw_wassserrettung", [ 64, 65 ] ],
+    [ "gw_taucher", [ 63, 69 ] ],
+    [ "seg_rescue_dogs", [ 91 ] ],
+    [ "intensive_care", [ 97 ] ],
+    [ "police_einsatzleiter", [ 35 ] ],
+    [ "police_fukw", [ 51 ] ],
+    [ "polizeihubschrauber", [ 61 ] ],
+    [ "police_wasserwerfer", [ 72 ] ],
+    [ "police_sek", [ 79, 80 ] ],
+    [ "police_mek", [ 81, 82 ] ],
+    [ "k9", [ 94 ] ],
+    [ "police_motorcycle", [ 95 ] ],
+    [ "police_firefighting", [ 96 ] ],
+    [ "thw_zugtrupp", [ 40 ] ],
+    [ "thw_raumen", [ 42, 45 ] ],
+    [ "thw_rescue_dogs", [ 93 ] ]
+];
 (function ()
 {
     'use strict';
@@ -19,40 +58,6 @@
         {
             element.addEventListener("click", function ()
             {
-                const educationArray = [
-                    [ "gw_messtechnik", [ 12 ] ],
-                    [ "gw_gefahrgut", [ 27, 77 ] ],
-                    [ "gw_hoehenrettung", [ 33 ] ],
-                    [ "elw2", [ 34, 78 ] ],
-                    [ "wechsellader", [ 46 ] ],
-                    [ "dekon_p", [ 53, 54 ] ],
-                    [ "fwk", [ 57 ] ],
-                    [ "arff", [ 75 ] ],
-                    [ "rettungstreppe", [ 76 ] ],
-                    [ "werkfeuerwehr", [ 83, 85, 86 ] ],
-                    [ "notarzt", [ 29, 73, 74, 97 ] ],
-                    [ "lna", [ 55 ] ],
-                    [ "orgl", [ 56 ] ],
-                    [ "seg_elw", [ 59 ] ],
-                    [ "seg_gw_san", [ 60 ] ],
-                    [ "gw_wassserrettung", [ 64, 65 ] ],
-                    [ "gw_taucher", [ 63, 69 ] ],
-                    [ "seg_rescue_dogs", [ 91 ] ],
-                    [ "intensive_care", [ 97 ] ],
-                    [ "police_einsatzleiter", [ 35 ] ],
-                    [ "police_fukw", [ 51 ] ],
-                    [ "polzeihubschrauber", [ 61 ] ],
-                    [ "police_wassserwerfer", [ 72 ] ],
-                    [ "police_sek", [ 79, 80 ] ],
-                    [ "police_mek", [ 81, 82 ] ],
-                    [ "k9", [ 94 ] ],
-                    [ "police_motorcycle", [ 95 ] ],
-                    [ "police_firefighting", [ 96 ] ],
-                    [ "thw_zugtrupp", [ 40 ] ],
-                    [ "thw_raumen", [ 42, 45 ] ],
-                    [ "thw_rescue_dogs", [ 93 ] ]
-                ];
-
                 if (element.checked)
                 {
                     for (let a = 0; a < educationArray.length; a++)
@@ -62,13 +67,14 @@
                         {
                             for (let b = 0; b < educationArray[ a ][ 1 ].length; b++)
                             {
-                                showVehicles(educationArray[ a ][ 1 ][ b ], element.attributes.value);
+                                showVehicles(educationArray[ a ][ 1 ][ b ], element.attributes.education_key.value);
                             }
                             break;
                         }
                         else 
                         {
                             cleanUp();
+                            educationTypeGlobal = null;
                         }
                     }
                 }
@@ -85,14 +91,8 @@
 */
 function showVehicles(vehicleId, educationType)
 {
-    cleanUp(educationType);
-
-    sessionStorage.setItem('ccCourseEducation', educationType);
-    if (!localStorage.aVehicles || JSON.parse(localStorage.aVehicles).lastUpdate < (new Date().getTime() - 5 * 1000 * 60) || JSON.parse(localStorage.aVehicles).userId != user_id)
-    {
-        $.getJSON('/api/vehicles').done(data => localStorage.setItem('aVehicles', JSON.stringify({ lastUpdate: new Date().getTime(), value: data, userId: user_id })));
-    }
-    var aVehicles = JSON.parse(localStorage.aVehicles).value;
+    cleanUp(educationTypeGlobal);
+    educationTypeGlobal = educationType;
 
     var wachen = document.getElementById("accordion").children;
     for (var i = 0; i < wachen.length; i++)
@@ -113,10 +113,10 @@ function showVehicles(vehicleId, educationType)
 //Delets span if a other education is selected
 function cleanUp(educationType)
 {
-    if (sessionStorage.getItem('ccCourseEducation') != educationType && sessionStorage.getItem('ccCourseEducation') != null)
+    if (educationTypeGlobal != educationType && educationTypeGlobal != null)
     {
         let labels = document.getElementsByClassName("label-info");
-        while (labels[ 0 ] != null)
+        while (labels.length > 0)
         {
             labels[ 0 ].remove();
         }
