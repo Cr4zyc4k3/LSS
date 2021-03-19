ea4b4c98d4a9a090e1e2aec8d2a95a8a865a4f99
Cr4zyc4k3 <69073372+Cr4zyc4k3@users.noreply.github.com>
2021-03-19 21:38:23 +0100

    course show added option

--- LSS_course_show_vehicle.user.js
+++ LSS_course_show_vehicle.user.js
@@ -10,101 +10,117 @@
 
 //education and the vehicles whom need them. de_DE only!
 const educationArray = [
-    [ "gw_messtechnik", [ 12 ] ],
-    [ "gw_gefahrgut", [ 27, 77 ] ],
-    [ "gw_hoehenrettung", [ 33 ] ],
-    [ "elw2", [ 34, 78 ] ],
-    [ "wechsellader", [ 46 ] ],
-    [ "dekon_p", [ 53, 54 ] ],
-    [ "fwk", [ 57 ] ],
-    [ "arff", [ 75 ] ],
-    [ "rettungstreppe", [ 76 ] ],
-    [ "werkfeuerwehr", [ 83, 85, 86 ] ],
-    [ "notarzt", [ 29, 31, 73, 74, 97 ] ],
-    [ "lna", [ 55 ] ],
-    [ "orgl", [ 56 ] ],
-    [ "seg_elw", [ 59 ] ],
-    [ "seg_gw_san", [ 60 ] ],
-    [ "gw_wasserrettung", [ 64, 65 ] ],
-    [ "gw_taucher", [ 63, 69 ] ],
-    [ "seg_rescue_dogs", [ 91 ] ],
-    [ "intensive_care", [ 97 ] ],
-    [ "police_einsatzleiter", [ 35 ] ],
-    [ "police_fukw", [ 51 ] ],
-    [ "polizeihubschrauber", [ 61 ] ],
-    [ "police_wasserwerfer", [ 72 ] ],
-    [ "police_sek", [ 79, 80 ] ],
-    [ "police_mek", [ 81, 82 ] ],
-    [ "k9", [ 94 ] ],
-    [ "police_motorcycle", [ 95 ] ],
-    [ "police_firefighting", [ 96 ] ],
-    [ "thw_zugtrupp", [ 40 ] ],
-    [ "thw_raumen", [ 42, 45 ] ],
-    [ "thw_rescue_dogs", [ 93 ] ]
+    ["gw_messtechnik", [12]],
+    ["gw_gefahrgut", [27, 77]],
+    ["gw_hoehenrettung", [33]],
+    ["elw2", [34, 78]],
+    ["wechsellader", [46]],
+    ["dekon_p", [53, 54]],
+    ["fwk", [57]],
+    ["arff", [75]],
+    ["rettungstreppe", [76]],
+    ["werkfeuerwehr", [83, 85, 86]],
+    ["notarzt", [29, 31, 73, 74, 97]],
+    ["lna", [55]],
+    ["orgl", [56]],
+    ["seg_elw", [59]],
+    ["seg_gw_san", [60]],
+    ["gw_wasserrettung", [64, 65]],
+    ["gw_taucher", [63, 69]],
+    ["seg_rescue_dogs", [91]],
+    ["intensive_care", [97]],
+    ["police_einsatzleiter", [35]],
+    ["police_fukw", [51]],
+    ["polizeihubschrauber", [61]],
+    ["police_wasserwerfer", [72]],
+    ["police_sek", [79, 80]],
+    ["police_mek", [81, 82]],
+    ["k9", [94]],
+    ["police_motorcycle", [95]],
+    ["police_firefighting", [96]],
+    ["thw_zugtrupp", [40]],
+    ["thw_raumen", [42, 45]],
+    ["thw_rescue_dogs", [93]]
 ];
 const educationArrayVB = [
-    [ "GW-Messtechnik Lehrgang", [ 12 ] ],
-    [ "GW-Gefahrgut Lehrgang", [ 27, 77 ] ],
-    [ "Höhenrettung Lehrgang", [ 33 ] ],
-    [ "ELW 2 Lehrgang", [ 34, 78 ] ],
-    [ "Wechsellader Lehrgang", [ 46 ] ],
-    [ "Dekon-P Lehrgang", [ 53, 54 ] ],
-    [ "Feuerwehrkran Lehrgang", [ 57 ] ],
-    [ "Flugfeldlöschfahrzeug-Ausbildung", [ 75 ] ],
-    [ "Rettungstreppen-Ausbildung", [ 76 ] ],
-    [ "Werkfeuerwehr-Ausbildung", [ 83, 85, 86 ] ],
-    [ "Notarzt-Ausbildung", [ 29, 31, 73, 74, 97 ] ],
-    [ "LNA-Ausbildung", [ 55 ] ],
-    [ "OrgL-Ausbildung", [ 56 ] ],
-    [ "SEG - Einsatzleitung", [ 59 ] ],
-    [ "SEG - GW-San", [ 60 ] ],
-    [ "GW-Wasserrettung Lehrgang", [ 64, 65 ] ],
-    [ "GW-Taucher Lehrgang", [ 63, 69 ] ],
-    [ "Rettungshundeführer (SEG)", [ 91 ] ],
-    [ "Intensivpflege", [ 97 ] ],
-    [ "Zugführer (leBefKw)", [ 35 ] ],
-    [ "Hundertschaftsführer (FüKw)", [ 51 ] ],
-    [ "Polizeihubschrauber", [ 61 ] ],
-    [ "Wasserwerfer", [ 72 ] ],
-    [ "SEK", [ 79, 80 ] ],
-    [ "MEK", [ 81, 82 ] ],
-    [ "Hundeführer (Schutzhund)", [ 94 ] ],
-    [ "Motorradstaffel", [ 95 ] ],
-    [ "Brandbekämpfung", [ 96 ] ],
-    [ "Zugtrupp", [ 40 ] ],
-    [ "Fachgruppe Räumen", [ 42, 45 ] ],
-    [ "Wassergefahren Lehrgang", [ 64, 65 ] ],
-    [ "Bergungstaucher Lehrgang", [ 63, 69 ] ],
-    [ "Rettungshundeführer (THW)", [ 93 ] ]
+    ["GW-Messtechnik Lehrgang", [12]],
+    ["GW-Gefahrgut Lehrgang", [27, 77]],
+    ["Höhenrettung Lehrgang", [33]],
+    ["ELW 2 Lehrgang", [34, 78]],
+    ["Wechsellader Lehrgang", [46]],
+    ["Dekon-P Lehrgang", [53, 54]],
+    ["Feuerwehrkran Lehrgang", [57]],
+    ["Flugfeldlöschfahrzeug-Ausbildung", [75]],
+    ["Rettungstreppen-Ausbildung", [76]],
+    ["Werkfeuerwehr-Ausbildung", [83, 85, 86]],
+    ["Notarzt-Ausbildung", [29, 31, 73, 74, 97]],
+    ["LNA-Ausbildung", [55]],
+    ["OrgL-Ausbildung", [56]],
+    ["SEG - Einsatzleitung", [59]],
+    ["SEG - GW-San", [60]],
+    ["GW-Wasserrettung Lehrgang", [64, 65]],
+    ["GW-Taucher Lehrgang", [63, 69]],
+    ["Rettungshundeführer (SEG)", [91]],
+    ["Intensivpflege", [97]],
+    ["Zugführer (leBefKw)", [35]],
+    ["Hundertschaftsführer (FüKw)", [51]],
+    ["Polizeihubschrauber", [61]],
+    ["Wasserwerfer", [72]],
+    ["SEK", [79, 80]],
+    ["MEK", [81, 82]],
+    ["Hundeführer (Schutzhund)", [94]],
+    ["Motorradstaffel", [95]],
+    ["Brandbekämpfung", [96]],
+    ["Zugtrupp", [40]],
+    ["Fachgruppe Räumen", [42, 45]],
+    ["Wassergefahren Lehrgang", [64, 65]],
+    ["Bergungstaucher Lehrgang", [63, 69]],
+    ["Rettungshundeführer (THW)", [93]]
 ];
+var showOnlyExistence;
 
 
-(function ()
-{
+(function () {
 
     'use strict';
+   
+    if (localStorage.getItem("ccCourseShow")!=null) {
+        showOnlyExistence = localStorage.getItem("ccCourseShow");
+        
+    }
+    else{
+        showOnlyExistence = false;
+        localStorage.setItem("ccCourseShow", false);
+    }
+    
+    document.querySelectorAll(".alert", ".alert-info")[0].insertAdjacentHTML("afterend","<div class='alert alert-info' id='ccShowCourseOptionDiv'><input type='checkbox' id='ccShowCourseOptionCheckbox'> Nur Anzeigen, ob Fahrzeug(e) existieren.</div>");
+    document.getElementById("ccShowCourseOptionCheckbox").addEventListener("change", function(){
+        if(document.getElementById("ccShowCourseOptionCheckbox").checked)
+        {
+            showOnlyExistence = true;
+            localStorage.setItem("ccCourseShow", true);
+        }
+        else{
+            showOnlyExistence = false;
+            localStorage.setItem("ccCourseShow", false);
+        }
+    })
 
     //check whether this is a building and a school
-    if (window.location.pathname.split("/")[ 1 ] == "buildings" && document.getElementById("schooling") != null)
-    {
+    if (window.location.pathname.split("/")[1] == "buildings" && document.getElementById("schooling") != null) {
         //get all input education buttons
         const inputs = document.querySelectorAll("input[id^=education_]");
-        inputs.forEach(element =>
-        {
+        inputs.forEach(element => {
             //Adding eventlistener to each input
-            element.addEventListener("click", function ()
-            {
+            element.addEventListener("click", function () {
                 //checking whether this input is really checked
-                if (element.checked)
-                {
+                if (element.checked) {
                     //delete all existing labels
                     cleanUp();
                     let education = element.attributes.education_key.value;
-                    for (let i = 0; i < educationArray.length; i++)
-                    {
-                        if (education == educationArray[ i ][ 0 ])
-                        {
-                            var educationIDs = educationArray[ i ][ 1 ];
+                    for (let i = 0; i < educationArray.length; i++) {
+                        if (education == educationArray[i][0]) {
+                            var educationIDs = educationArray[i][1];
                             break;
                         }
 
@@ -116,15 +132,16 @@ const educationArrayVB = [
         });
 
     }
-    else if (window.location.pathname.split("/")[ 1 ] == "schoolings" && document.getElementById("accordion") != null)
-    {
+    //URL is /schooling
+    else if (window.location.pathname.split("/")[1] == "schoolings" && document.getElementById("accordion") != null) {
+        //delete all exisitng labels
         cleanUp();
-        let education = document.getElementsByTagName("h2")[ 0 ].innerHTML;
-        for (let i = 0; i < educationArrayVB.length; i++)
-        {
-            if (education == educationArrayVB[ i ][ 0 ])
-            {
-                var educationIDs = educationArrayVB[ i ][ 1 ];
+        //get education
+        let education = document.getElementsByTagName("h2")[0].innerHTML;
+        //check educationtype in array
+        for (let i = 0; i < educationArrayVB.length; i++) {
+            if (education == educationArrayVB[i][0]) {
+                var educationIDs = educationArrayVB[i][1];
                 break;
             }
 
@@ -135,45 +152,53 @@ const educationArrayVB = [
 
 })();
 
-function cleanUp()
-{
+//delete all labels
+function cleanUp() {
+    //get all labels
     let vehicleLabel = document.querySelectorAll("[id^=ccShowCourse_");
-    vehicleLabel.forEach(element =>
-    {
+    vehicleLabel.forEach(element => {
         element.remove();
     });
 }
-async function showVehicles(vehicleIDArray)
-{
-    try
-    {
-        var buildings = [].slice.call(document.getElementById("accordion").children);
-        if (!sessionStorage.aVehicles || JSON.parse(sessionStorage.aVehicles).lastUpdate < (new Date().getTime() - 5 * 1000 * 60) || JSON.parse(sessionStorage.aVehicles).userId != user_id)
-        {
+
+//show vehicles async for call at API
+async function showVehicles(vehicleIDArray) {
+
+    //Try-Catch for timeout/overflow
+    try {
+        if (!sessionStorage.aVehicles || JSON.parse(sessionStorage.aVehicles).lastUpdate < (new Date().getTime() - 5 * 1000 * 60) || JSON.parse(sessionStorage.aVehicles).userId != user_id) {
             await $.getJSON('/api/vehicles').done(data => sessionStorage.setItem('aVehicles', JSON.stringify({ lastUpdate: new Date().getTime(), value: data, userId: user_id })));
         }
         var aVehicles = JSON.parse(sessionStorage.aVehicles).value;
-    } catch (error)
-    {
+    } catch (error) {
         console.error("Cant read Vehicle data");
     }
     var buildings = [].slice.call(document.getElementById("accordion").children);
 
-    for (let j = 0; j < vehicleIDArray.length; j++)
-    {
-        for (let l = 0; l < buildings.length; l++)
-        {
-            let buildingsChild = buildings[ l ].firstElementChild
-
-            for (let k = 0; k < aVehicles.length; k++)
-            {
-                if (buildingsChild.attributes.building_id.value == aVehicles[ k ].building_id && vehicleIDArray[ j ] == aVehicles[ k ].vehicle_type)
-                {
-                    var span = document.createElement("span");
-                    span.id = "ccShowCourse_" + aVehicles[ k ].id;
-                    span.classList.add("label", "label-info");
-                    span.innerText = aVehicles[ k ].caption;
-                    buildingsChild.firstElementChild.appendChild(span);
+    //1. loop for all vehicles who need this education
+    loop1: for (let j = 0; j < vehicleIDArray.length; j++) {
+        //2. Loop for all buildings availible
+        loop2: for (let l = 0; l < buildings.length; l++) {
+            let buildingsChild = buildings[l].firstElementChild
+            //3. Loop for all vehicles 
+            loop3: for (let k = 0; k < aVehicles.length; k++) {
+                if (buildingsChild.attributes.building_id.value == aVehicles[k].building_id && vehicleIDArray[j] == aVehicles[k].vehicle_type) {
+                    if (!showOnlyExistence) {
+                        var span = document.createElement("span");
+                        span.id = "ccShowCourse_" + aVehicles[k].id;
+                        span.classList.add("label", "label-info");
+                        span.innerText = aVehicles[k].caption;
+                        buildingsChild.firstElementChild.appendChild(span);
+                    }
+                    else {
+                        span = document.createElement("span");
+                        span.id = "ccShowCourse_" + aVehicles[k].id;
+                        span.classList.add("label", "label-info");
+                        span.innerText = "Fahrzeug vorhanden"
+                        buildingsChild.firstElementChild.appendChild(span);
+                        //jump to next building
+                        continue loop2;
+                    }
                 }
             }
         }
