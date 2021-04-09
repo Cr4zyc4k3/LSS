02c4f079c0fb8a570cc7ebd5acec81100907d7c6
Cr4zyc4k3 <69073372+Cr4zyc4k3@users.noreply.github.com>
2021-04-09 09:42:58 +0200

    ✨ hide missions added min/max

--- LSS_hide_missions.user.js
+++ LSS_hide_missions.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        LSS hide missions
-// @version     1.0
+// @version     1.1
 // @author      Crazycake
 // @include     /^https?:\/\/(?:w{3}\.)?(?:(policie\.)?operacni-stredisko\.cz|(politi\.)?alarmcentral-spil\.dk|(polizei\.)?leitstellenspiel\.de|(?:(police\.)?missionchief-australia|(police\.)?missionchief|(poliisi\.)?hatakeskuspeli|missionchief-japan|missionchief-korea|(politiet\.)?nodsentralspillet|(politie\.)?meldkamerspel|operador193|(policia\.)?jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|(police\.)?missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|(police\.)?operateur112\.fr|(polizia\.)?operatore112\.it|(policja\.)?operatorratunkowy\.pl|dispetcher112\.ru|(polis\.)?larmcentralen-spelet\.se)\/.*$/
 // @grant       none
@@ -12,41 +12,62 @@
 
 
 	//default credits
-	var creditsMin
-	if (localStorage.getItem("ccHideMissions") != null)
+	var creditsMin;
+	var creditsMax;
+	if (localStorage.getItem("ccHideMissionsMin") != null && localStorage.getItem("ccHideMissionsMax"))
 	{
-		creditsMin = localStorage.getItem("ccHideMissions");
-		sortList(creditsMin);
+		creditsMin = localStorage.getItem("ccHideMissionsMin");
+		creditsMax = localStorage.getItem("ccHideMissionsMax");
+		sortList(creditsMin, creditsMax);
 	}
 	else
 	{
 		creditsMin = 3000;
-		localStorage.setItem("ccHideMissions", creditsMin);
+		creditsMax = 12000
+		localStorage.setItem("ccHideMissionsMin", creditsMin);
+		localStorage.setItem("ccHideMissionsMax", creditsMax);
 	}
 
 
 	//input for custom credit value
 	$('#navbar_profile_link').parent().after(`<li role="presentation">
 												<div style ="padding: 3px 20px;">
-													Ausblenden bis: <input id="ccHideMissions" type="number" style="width:100px;" placeholder="3000">
+													Ausblenden bis: <input id="ccHideMissionsCreditsMin" type="number" style="width:100px;" placeholder="`+ creditsMin + `">
+													Ausblenden ab: <input id="ccHideMissionsCreditsMax" type="number" style="width:100px;" placeholder="`+ creditsMax + `">
 												</div>
 											</li>`);
 
 
-	//Eventlistener for focusout on checkbox
-	$('#ccHideMissions').focusout(function ()
+	//Eventlistener for focusout on checkboxes
+	$('[id^=ccHideMissionsCreditsM]').focusout(function ()
 	{
+
 		//check whether input is number
-		if (!Number.isNaN($('#ccHideMissions').val()) && $('#ccHideMissions').val() != creditsMin)
+		if (!Number.isNaN($('#ccHideMissionsCreditsMin').val()) && !Number.isNaN($('#ccHideMissionsCreditsMax').val()))
 		{
-			creditsMin = this.value;
-			sortList(creditsMin);
-			localStorage.setItem("ccHideMissions", creditsMin);
+			creditsMin = $('#ccHideMissionsCreditsMin').val()
+			creditsMax = $('#ccHideMissionsCreditsMax').val()
+			localStorage.setItem("ccHideMissionsMin", creditsMin);
+			localStorage.setItem("ccHideMissionsMax", creditsMax);
+			sortList(creditsMin, creditsMax);
 		}
 	})
 	//get own missions
-	async function sortList(creditsMin)
+	async function sortList(creditsMin, creditsMax)
 	{
+		//Remove all hides
+		var missions = $('#mission_list').children();
+		for (let i = 0; i < missions.length; i++)
+		{
+			//exclude the "no emergency"
+			if (missions[ i ].id == "emergency_no")
+			{
+				missions[ i ].classList.add('hide');
+				continue;
+			}
+			missions[ i ].classList.remove('hide');
+
+		}
 		//default request for einsaetze.json
 		if (!localStorage.aMissions || JSON.parse(localStorage.aMissions).lastUpdate < (new Date().getTime() - 5 * 1000 * 60))
 		{
@@ -55,14 +76,13 @@
 		var aMissions = JSON.parse(localStorage.aMissions).value;
 
 
-		var missions = $('#mission_list').children();
 		for (let i = 0; i < missions.length; i++)
 		{	//remove the "no missions-info"
 			if (missions[ i ].attributes.mission_id != null)
 			{	//compare to missions API
 				for (let j = 0; j < aMissions.length; j++)
 				{	//if avarge_credits < credtisMin hide them
-					if (missions[ i ].attributes.mission_type_id.value == aMissions[ j ].id && aMissions[ j ].average_credits < creditsMin)
+					if (missions[ i ].attributes.mission_type_id.value == aMissions[ j ].id && (aMissions[ j ].average_credits < creditsMin || aMissions[ j ].average_credits > creditsMax))
 					{
 						missions[ i ].classList.add('hide');
 						//break for faster work
