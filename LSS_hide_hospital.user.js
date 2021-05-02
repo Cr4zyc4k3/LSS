1c435f6ee20fb324d9eb908f57ce1b9135d745ec
Mike <69073372+Cr4zyc4k3@users.noreply.github.com>
2021-05-02 13:59:01 +0200

    adapeted to game change (hide full hospitals)

--- LSS_hide_hospital.user.js
+++ LSS_hide_hospital.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        LSS hide hospital
-// @version     1.1
+// @version     1.2
 // @author      Crazycake
 // @include     /^https?:\/\/(?:w{3}\.)?(?:operacni-stredisko\.cz|alarmcentral-spil\.dk|leitstellenspiel\.de|missionchief\.gr|(?:missionchief-australia|missionchief|hatakeskuspeli|missionchief-japan|missionchief-korea|nodsentralspillet|meldkamerspel|operador193|jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|operateur112\.fr|operatore112\.it|operatorratunkowy\.pl|dispetcher112\.ru|larmcentralen-spelet\.se)\/vehicles\/\d*$
 // @grant       none
@@ -12,7 +12,7 @@
 	const ccRTWKHList = [
 		{
 			name: 'STO',
-			RTW: ['10327783'],
+			RTW: ['10101554','10327789','12125863'],
 			KH: ['2917725', '4603995'],
 		},
 		{
@@ -37,7 +37,7 @@
 					.parent()
 					.addClass('ccNoDel');
 			});
-			$('h4').first().next().children().last().children().not('.ccNoDel').hide();
+			$('tr').not('.ccNoDel').hide();
 		}
 	}
 })();
